ec6842e176a6651695f49d4c8e6b4a859c7acb60
Harsh K Desai <harshkdesai109@yahoo.com>
2018-04-04 21:05:12 -0400

    Q4 updated
    Bug fixes

--- jenkins/core/src/main/java/jenkins/model/IdStrategy.java
+++ jenkins/core/src/main/java/jenkins/model/IdStrategy.java
@@ -240,13 +240,15 @@ public abstract class IdStrategy extends AbstractDescribableImpl<IdStrategy> imp
                     } else if ('_' == c || '.' == c || '-' == c || ' ' == c || '@' == c) {
                         buf.append(c);
                     } else if (c == '~') {
-                    	for(int j = 0; j < 4; j++) {
-                    		i++;
-                    		if (i < chars.length) {
-                    			hex.append(chars[i]);
-                    		} else {
-                    			break;
-                    		}
+                        StringBuilder hex = new StringBuilder(4);
+                        for(int j = 0; j < 4; j++) {
+                            i++;
+                            if (i < chars.length) {
+                                hex.append(chars[i]);
+                            } else {
+                                break;
+                            } 
+                        }
                         buf.append(Character.valueOf((char)Integer.parseInt(hex.toString(), 16)));
                     }
                 }
